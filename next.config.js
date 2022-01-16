ef6682969c2aeda3fd5dbe87eb9100cd4466d844
Gal Schlezinger <gal@spitfire.co.il>
2022-01-16 14:36:26 +0200

    bundles

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const withTM = require("next-transpile-modules")([
-  "react-monaco-editor",
-  "monaco-editor",
-]);
-
-module.exports = withTM({});
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
-import dynamic from "next/dynamic";
 import MagicString from "magic-string";
-import { useEffect, useState } from "react";
-import { MonacoEditorProps } from "react-monaco-editor";
+import { useEffect, useRef, useState } from "react";
 import Head from "next/head";
-
-const MonacoEditor = dynamic(() => import("react-monaco-editor"), {
-  ssr: false,
-});
+import MonacoEditor, {
+  EditorProps,
+  useMonaco,
+  Monaco,
+} from "@monaco-editor/react";
+// @ts-ignore
+import bundled from "!raw-loader!../magic-string-playground.d.ts";
 
 async function evaluate(code: string, input: string): Promise<string> {
   try {
-    const fn = eval(`(async function(ms, original) { ${code} })`);
+    const fn = new Function("ms", "original", code);
     const ms = new MagicString(input);
     await fn(ms, input);
     const map = ms.generateMap({
@@ -23,31 +24,53 @@ async function evaluate(code: string, input: string): Promise<string> {
     ].join("\n");
     return newOutput;
   } catch (e) {
-    console.error(e);
-    return "";
+    throw e;
   }
 }
 
 const INITIAL_CODE = `// Type your code here`;
-const INITIAL_TRANSFORMATION = `// Type your transformation here
-// You can use the following values:
-// * ms - the MagicString instance
-// * original - the original input
+const INITIAL_TRANSFORMATION = `
+/**
+  * Type your transformation here
+  * You can use the following values:
+  *
+  * @param ms - The MagicString instance
+  * @param original - The original input
+  */
 
 ms.append(". This is amazing!");
-`;
+`.trim();
 
 export default function Home() {
   let [code, setCode] = useState(INITIAL_CODE);
   let [transformation, setTransformation] = useState(INITIAL_TRANSFORMATION);
   let [output, setOutput] = useState("");
+  let [isErrored, setIsErrored] = useState<string>();
+  const monaco = useMonaco();
+  const ref = useRef(false);
+
+  if (monaco && !ref.current) {
+    ref.current = true;
+    setupSources(monaco);
+  }
 
   useEffect(() => {
-    try {
-      evaluate(transformation, code).then(setOutput, console.error);
-    } catch (e) {
-      console.error(e);
+    if (monaco) {
+      return setupSources(monaco);
     }
+  }, [monaco]);
+
+  useEffect(() => {
+    evaluate(transformation, code).then(
+      (v) => {
+        setOutput(v);
+        setIsErrored(undefined);
+      },
+      (err) => {
+        console.error(err);
+        setIsErrored(String(err.message));
+      }
+    );
   }, [transformation, code]);
 
   return (
@@ -55,21 +78,38 @@ export default function Home() {
       <Head>
         <title>Magic String playground</title>
       </Head>
-      <Editor containerClassName="div1" value={code} onChange={setCode} />
       <Editor
+        options={{
+          suggest: { preview: false },
+          suggestOnTriggerCharacters: false,
+          hover: { enabled: false },
+          quickSuggestions: false,
+        }}
+        containerClassName="div1"
+        value={code}
+        onChange={(c) => c && setCode(c)}
+      />
+      <Editor
+        errorMessage={isErrored}
         containerClassName="div2"
         value={transformation}
-        onChange={setTransformation}
+        onChange={(v) => v && setTransformation(v)}
       />
       <Editor
         containerClassName="div3"
         value={output}
-        options={{ readOnly: true }}
+        options={{
+          readOnly: true,
+          hover: { enabled: false },
+        }}
       />
       <style global jsx>{`
         body {
           margin: 0;
           padding: 0;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+            "Helvetica Neue", sans-serif;
         }
 
         .parent {
@@ -80,6 +120,10 @@ export default function Home() {
 
           min-height: 100vh;
           min-width: 100vw;
+        }
+
+        .errored {
+          border: 2px solid red;
         }
 
         .div1 {
@@ -96,14 +140,18 @@ export default function Home() {
   );
 }
 
-function Editor(props: MonacoEditorProps & { containerClassName?: string }) {
+function Editor(
+  props: EditorProps & { containerClassName?: string; errorMessage?: string }
+) {
   return (
-    <div className={props.containerClassName}>
+    <div className={props.containerClassName} style={{ position: "relative" }}>
+      {props.errorMessage && (
+        <div className="error-message">{props.errorMessage}</div>
+      )}
       <MonacoEditor
         height="100%"
         width="100%"
         language="javascript"
-        theme="vs-dark"
         {...props}
         options={{
           minimap: {
@@ -112,6 +160,31 @@ function Editor(props: MonacoEditorProps & { containerClassName?: string }) {
           ...props.options,
         }}
       />
+      <style jsx>{`
+        .error-message {
+          color: red;
+          background-color: #ffcdd2;
+          padding: 0.5em;
+          border-radius: 0.5em;
+          position: absolute;
+          left: 0.5em;
+          bottom: 0.5em;
+          z-index: 1000;
+        }
+      `}</style>
     </div>
   );
+}
+
+function setupSources(monaco: Monaco) {
+  const v = monaco.languages.typescript.javascriptDefaults.addExtraLib(`
+    ${bundled}
+
+    declare global {
+      declare const ms: MagicString;
+      declare const original: string;
+    }
+  `);
+
+  return () => v.dispose();
 }
