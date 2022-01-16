35b11fb10783decc8360ba85cbbcff11d4944de3
Gal Schlezinger <gal@spitfire.co.il>
2022-01-16 12:43:48 +0200

    Add title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import MagicString from "magic-string";
 import { useEffect, useState } from "react";
+import { MonacoEditorProps } from "react-monaco-editor";
+import Head from "next/head";
 
 const MonacoEditor = dynamic(() => import("react-monaco-editor"), {
   ssr: false,
@@ -26,9 +28,18 @@ async function evaluate(code: string, input: string): Promise<string> {
   }
 }
 
+const INITIAL_CODE = `// Type your code here`;
+const INITIAL_TRANSFORMATION = `// Type your transformation here
+// You can use the following values:
+// * ms - the MagicString instance
+// * original - the original input
+
+ms.append(". This is amazing!");
+`;
+
 export default function Home() {
-  let [code, setCode] = useState("");
-  let [transformation, setTransformation] = useState("");
+  let [code, setCode] = useState(INITIAL_CODE);
+  let [transformation, setTransformation] = useState(INITIAL_TRANSFORMATION);
   let [output, setOutput] = useState("");
 
   useEffect(() => {
@@ -40,44 +51,65 @@ export default function Home() {
   }, [transformation, code]);
 
   return (
-    <div>
-      <MonacoEditor
-        width="800"
-        height="600"
-        language="javascript"
-        theme="vs-dark"
-        value={code}
-        options={{
-          minimap: {
-            enabled: false,
-          },
-        }}
-        onChange={setCode}
-      />
-      <MonacoEditor
-        width="800"
-        height="600"
-        language="javascript"
-        theme="vs-dark"
+    <div className="parent">
+      <Head>
+        <title>Magic String playground</title>
+      </Head>
+      <Editor containerClassName="div1" value={code} onChange={setCode} />
+      <Editor
+        containerClassName="div2"
         value={transformation}
-        options={{
-          minimap: {
-            enabled: false,
-          },
-        }}
         onChange={setTransformation}
       />
+      <Editor
+        containerClassName="div3"
+        value={output}
+        options={{ readOnly: true }}
+      />
+      <style global jsx>{`
+        body {
+          margin: 0;
+          padding: 0;
+        }
+
+        .parent {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          grid-template-rows: repeat(2, 1fr);
+          grid-gap: 0.2em;
+
+          min-height: 100vh;
+          min-width: 100vw;
+        }
+
+        .div1 {
+          grid-area: 1 / 1 / 2 / 2;
+        }
+        .div2 {
+          grid-area: 2 / 1 / 3 / 2;
+        }
+        .div3 {
+          grid-area: 1 / 2 / 3 / 3;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+function Editor(props: MonacoEditorProps & { containerClassName?: string }) {
+  return (
+    <div className={props.containerClassName}>
       <MonacoEditor
-        width="800"
-        height="600"
+        height="100%"
+        width="100%"
         language="javascript"
         theme="vs-dark"
-        value={output}
+        {...props}
         options={{
-          readOnly: true,
           minimap: {
             enabled: false,
           },
+          ...props.options,
         }}
       />
     </div>
